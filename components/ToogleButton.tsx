6a73ff75f8317a68f2338828e85265c2a60cb703
unknown <anas.452111@gmail.com>
2023-07-28 10:40:27 +0500

    Hydration issue resolved

--- components/ToogleButton.tsx
+++ components/ToogleButton.tsx
@@ -1,53 +1,38 @@
-'use client'
+"use client"
 import React from 'react'
 import { useTheme } from 'next-themes'
-import { useState } from 'react'
 import { BsSun, BsMoon } from 'react-icons/bs'
 
 export default function ToogleButton() {
-  const {  theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
   return (
     <>
       <button
         onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
-        className={`relative rounded-full flex justify-between items-center w-[80px] h-[40px] py-1.5 px-3 ${theme=="light"?"shadow-2xl":"shadow-toogle"}`}
+        className={`relative rounded-full flex justify-between items-center w-[80px] h-[40px] py-1.5 px-3 ${
+          theme == 'light' ? 'shadow-2xl' : 'shadow-toogle'
+        }`}
       >
-        <button
-         
+        <div
           className={`h-[35px] w-[35px] rounded-full absolute my-auto z-0 ${
-            theme == 'light' ? 'bg-yellow-300 -translate-x-2' : 'bg-black translate-x-7 '
+            theme == 'light'
+              ? 'bg-yellow-300 -translate-x-2'
+              : 'bg-black translate-x-7 '
           } transition-all duration-300 transform `}
         />
         <BsSun
           className={`z-10 h-[20px] w-auto ${
             theme == 'light' ? 'text-black ' : 'text-yellow-400'
           }`}
-         
         />
 
         <BsMoon
           className={`z-10 h-[20px] w-auto ${
             theme == 'light' ? 'text-black' : 'text-white'
           }`}
-          
         />
       </button>
     </>
-    // <Switch
-    // checked={enabled}
-    // onChange={setEnabled}
-    // onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
-    //   className={`${
-    //     enabled ? 'bg-black' : 'bg-gray-200'
-    //   } relative inline-flex h-6 w-11 items-center rounded-full`}
-    // >
-    //   <span className="sr-only">Enable notifications</span>
-    //   <span
-    //     className={`${
-    //       enabled ? 'translate-x-6' : 'translate-x-1'
-    //     } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-    //   />
-    // </Switch>
   )
 }
